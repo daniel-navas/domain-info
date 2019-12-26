e6bfb277058508cdab4b91e67467024f9b4ef7f8
Daniel Navas <danielnavasm@gmail.com>
2019-12-26 12:05:23 -0500

    implemented get title and logo func

--- handlers.go
+++ handlers.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/anaskhan96/soup"
 	"github.com/go-chi/chi"
 )
 
@@ -12,7 +14,7 @@ func getDomainInfo(w http.ResponseWriter, r *http.Request) {
 	url := chi.URLParam(r, "url")
 	resp, err := http.Get("https://api.ssllabs.com/api/v3/analyze?host=" + url)
 	if err != nil {
-		log.Fatalf("HTTP request failed. %s\n", err)
+		log.Printf("HTTP request failed. %s\n", err)
 	}
 	defer resp.Body.Close()
 	var rawDom rawDomainInfo
@@ -21,15 +23,37 @@ func getDomainInfo(w http.ResponseWriter, r *http.Request) {
 	for index, server := range domain.Severs {
 		resp, err := http.Get("http://ip-api.com/json/" + server.Address)
 		if err != nil {
-			log.Fatalf("HTTP request failed. %s\n", err)
+			log.Printf("HTTP request failed. %s\n", err)
 		}
 		defer resp.Body.Close()
 		var rawAddress rawAddressInfo
 		json.NewDecoder(resp.Body).Decode(&rawAddress)
 		domain.Severs[index] = mapAddressInfo(rawAddress, server)
 	}
+	fmt.Println(getTitleAndLogo(url))
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(domain)
 	w.WriteHeader(http.StatusOK)
+}
 
+func getTitleAndLogo(url string) [2]string {
+	resp, err := soup.Get("https://www.truora.com/")
+	if err != nil {
+		log.Printf("HTTP request failed. %s\n", err)
+	}
+	doc := soup.HTMLParse(resp)
+	title := doc.Find("title")
+	logo := doc.Find("link", "rel", "shortcut")
+	var titleAndLogo [2]string
+	if title.Error == nil {
+		titleAndLogo[0] = title.Text()
+	} else {
+		log.Println(title.Error)
+	}
+	if logo.Error == nil {
+		titleAndLogo[1] = logo.Attrs()["href"]
+	} else {
+		log.Println(title.Error)
+	}
+	return titleAndLogo
 }
